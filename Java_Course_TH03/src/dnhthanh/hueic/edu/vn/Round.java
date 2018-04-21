e213e31ca6f1e1a5381faf8507a297192c53962f
CuongDat <37949675+CuongDat@users.noreply.github.com>
2018-04-21 16:55:41 +0700

    Update Round.java

--- Java_Course_TH03/src/dnhthanh/hueic/edu/vn/Round.java
+++ Java_Course_TH03/src/dnhthanh/hueic/edu/vn/Round.java
@@ -1,4 +1,4 @@
-package dnhthanh.hueic.edu.vn;
+package cuongdat0508hue.gmail.com;
 
 public class Round {
 	private float R;//Bán kính
@@ -10,7 +10,18 @@ public class Round {
 	}
 	
 	//Tạo getters và setters cho R và O
-	
+	public float getR() {
+		return R;
+	}
+	public void setR(float R) {
+		this.R = R;
+	}
+	public Point getO() {
+		return O;
+	}
+	public void setO() {
+		this.O = O;
+	}
 	//Xét vị trí tương đối điểm với đường tròn: nếu nằm trong - trả về -1; nếu nằm ngoài - trả về 1; nếu nằm trên - trả về 0
 	public int ViTriTuongDoiDiemVoiDuongTron(Point A) {
 		
