5f4aaa22398dc1ca4554fbdb470fd31673051df4
CuongDat <37949675+CuongDat@users.noreply.github.com>
2018-04-30 22:19:19 +0700

    Update MainProgram.java

--- Java_Course_TH02/src/cuongdat0508hue/gmail/com/MainProgram.java
+++ Java_Course_TH02/src/cuongdat0508hue/gmail/com/MainProgram.java
@@ -1,28 +1,26 @@
-package cuongdat0508hue.gmail.com;
+papackage akimkute05.gmail.com;
 
 import java.util.Scanner;
 
 public class MainProgram {
 
 	public static void main(String[] args) {
-		String trangthai = new String();
-		HangHoa hh = new HangHoa();
+		String mua = new String();
 		KhachHang kh = new KhachHang();
-		//Tiếp tục với các yêu cầu của đề bài
+		// Tiếp tục với các yêu cầu của đề bài
 		Scanner sc = new Scanner(System.in);
 		int n;
 		System.out.println("Mời bạn nhập username");
 		String usn = sc.nextLine();
 		System.out.println("Mời bạn nhập password");
 		String pass = sc.nextLine();
-		if (usn.equals(kh.getUsername()) && pass.equals(kh.getPassword())){
-		do {
-			System.out.println("Bạn có muốn mua hàng");
-			System.out.println("1. Có");
-			System.out.println("2. Không");
-			n = sc.nextInt();
-			if (n == 1){
-					HangHoa hangHoa = new HangHoa();
+		if (usn.equals(kh.getUsername()) && pass.equals(kh.getPassword())) {
+			do {
+				System.out.println("Bạn có muốn mua hàng");
+				System.out.println("1. Có");
+				System.out.println("2. Không");
+				n = sc.nextInt();
+				if (n == 1) {
 					Scanner sc1 = new Scanner(System.in);
 					System.out.println("Nhập tên hàng");
 					String ten = sc1.nextLine();
@@ -32,14 +30,11 @@ public class MainProgram {
 					int sl = sc1.nextInt();
 					System.out.println("Nhập Đơn giá");
 					double dg = sc1.nextDouble();
-					trangthai += "\n------\n" + kh.MuaHang(ten, ngaysx, dg, sl);
-				}else {
-				System.out.println(trangthai);
-			}
-			}while (n != 2);
-		}else {
-			System.out.println("Xin lỗi, tài khoản của quý khách không tồn tại trong hệ thống. Xin liên hệ chi nhánh khách hàng gần nhất");
-		}
+					mua += "\n------\n" + kh.MuaHang(ten, ngaysx, dg, sl);
+				} else
+					System.out.println(mua);
+			} while (n == 1);
+		} else
+			System.out.println(" Xin lỗi, tài khoản của quý khách không tồn tại trong hệ thống. Xin liên hệ đăng ký với chi nhánh khách hàng gần nhất");
 	}
-
 }
